42024edc9dbfc1ddb0957fa5b73ec0ab6e5964ed
mstaflex <jasper.buesch@gmail.com>
2013-04-04 10:16:38 +0300

    Renamed

--- example.py
+++ example.py
@@ -5,8 +5,6 @@ import spi
 
 # This is a very simple script that uses the rPi SPI port to read out the various registers in a nRF24L01+ device
 # connected to the rPI SPI port 0
-# It should be pretty self-explanitory
-
 
 status = spi.openSPI(speed=1000000)
 print "SPI configuration = ", status
